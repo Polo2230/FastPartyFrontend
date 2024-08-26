fcddbf79bc8d16de57bbb809674138f767ce684c
Polo2230 <johans.polon@autonoma.edu.co>
2024-08-25 19:02:33 -0500

    TestFrontend1 Polo

---
+++ OneDrive/Documentos/frontend/1Proyecto/test/README.md
@@ -0,0 +1,5 @@
+# Programación frontend - 2024
+
+**Fecha:** 23 de agosto de 2024  
+
+**Estudiante:** Johan Steven Polo Nieto
---
+++ OneDrive/Documentos/frontend/1Proyecto/test/src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import {Principal} from './components/Principal'
+
+function App() {
+  return (
+   <>
+    <Header />
+    <Principal />
+    <Footer />
+   </>
+  )
+}
+
+export default App
